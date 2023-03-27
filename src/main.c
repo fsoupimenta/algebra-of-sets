e7af88bad7448506819e7b48da374b51978e3e49
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-03-27 17:03:56 -0300

    Create initial setup

---
+++ src/main.c
@@ -0,0 +1,43 @@
+//
+// Created by Fernando Pimenta on 25/03/2023.
+//
+
+#include "stdio.h"
+#include "locale.h"
+#define set_size 5
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int set_A[set_size] = {0, 1, 2, 3, 4},
+        set_B[set_size] = {4, 5, 6, 7, 8},
+        element, key = -1;
+
+    while (key != 0)
+    {
+        printf("What do you want to do? \n"
+               "1-)Print set A \n"
+               "2-)Print set B \n"
+               "0-)Exit \n");
+        scanf("%d", &key);
+
+        switch (key) {
+            case 1:
+                printf("One");
+                break;
+
+            case 2:
+                printf("Two");
+                break;
+
+            default:
+                break;
+        }
+        printf("\n ----------------------\n");
+    }
+
+    return 0;
+}
+
+
